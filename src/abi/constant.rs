14b032012cd129d845d5a301ed1df4667f2203c6
Cyborg-spec <cavidan.sultan@gmail.com>
2025-11-18 14:01:05 +0400

    Improvements

--- src/abi/constant.rs
+++ src/abi/constant.rs
@@ -1,10 +1,6 @@
 use alloy::primitives::{address, Address};
 
-#[allow(dead_code)]
 pub const MULLTICALL_ADDRESS: Address = address!("cA11bde05977b3631167028862bE2a173976CA11");
-#[allow(dead_code)]
 pub const USDT_ADDRESS: Address = address!("0xdAC17F958D2ee523a2206206994597C13D831ec7");
-#[allow(dead_code)]
 pub const USDC_ADDRESS: Address = address!("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
-#[allow(dead_code)]
 pub const POOL_USDC_USDT_ADDRESS: Address = address!("0x3416cF6C708Da44DB2624D63ea0AAef7113527C6");
